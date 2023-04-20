06ec7dc38468503e3dea6525730f1b88570ac0fa
free-flow-code <wildmuskaria8@gmail.com>
2023-04-20 17:29:18 +0400

    Добавил функцию для проверки наличия ID пользователя в таблице.

--- database/sqlite.py
+++ database/sqlite.py
@@ -1,7 +1,7 @@
 import sqlite3 as sq
 from dotenv import dotenv_values
 
-
+# Создание новой БД с таблицей orders
 async def db_start():
     global db, cursor
 
@@ -23,3 +23,8 @@ async def db_start():
                    'expiration_time DATETIME")'
                    )
     db.commit()
+
+
+# Проверка наличия user id в таблице. Возвращает True, если запись есть.
+async def is_user_id(user_id):
+    return cursor.execute(f'SELECT EXIST(SELECT {user_id} FROM orders)')
