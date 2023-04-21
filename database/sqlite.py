9703930fe2333e91062eec4982877a91b38f1b78
free-flow-code <wildmuskaria8@gmail.com>
2023-04-21 12:45:45 +0400

    Добавил функцию создания нового заказа в БД

--- database/sqlite.py
+++ database/sqlite.py
@@ -13,6 +13,13 @@ async def db_start():
     db.commit()
 
 
+# Создание нового заказа с заполнением поля user_id
+async def create_order(user_id):
+    user = cursor.execute(f'SELECT user_id FROM orders WHERE user_id == "{user_id}"')
+    if not user:
+        cursor.execute(f'INSERT INTO orders(user_id) VALUES({user_id})')
+        db.commit()
+
 # Проверка наличия user id в таблице. Возвращает True, если запись есть.
 async def is_user_id(user_id):
     return cursor.execute(f'SELECT EXIST(SELECT {user_id} FROM orders)')
