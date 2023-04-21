b95443428fd323ba6590db7dfd3e6f759296076a
Jorik-Laptop <mastakcompany@gmail.com>
2023-04-21 16:20:39 +0700

    Реализовал начальную логику ветки 'Отправить на хранение'

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,10 +1,13 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart, Text
 from aiogram import filters
-from aiogram.types import Message
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import Message, CallbackQuery
 
 from keyboards.user_keyboards import start_keyboard, \
-    storage_conditions_keyboard, what_can_be_stored_keyboard
+    storage_conditions_keyboard, what_can_be_stored_keyboard, \
+    send_to_storage_keyboard, item_weight_keyboard, item_dimensions_keyboard, \
+    rental_period_keyboard
 from lexicon.lexicon_ru import LEXICON_RU
 
 router: Router = Router()
@@ -43,3 +46,43 @@ async def process_storage_conditions_cmd(message: Message):
     )
 
 
+@router.message(Text(contains=['Отправить на хранение']))
+async def process_send_to_storage_cmd(message: Message):
+    await message.answer(
+        text=LEXICON_RU['rules'],
+        reply_markup=send_to_storage_keyboard()
+    )
+
+
+@router.callback_query(Text(text=['yourself', 'courier']))
+async def process_item_weight_cmd(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text='Каков примерный вес ваших вещей?',
+        reply_markup=item_weight_keyboard()
+    )
+    await callback.answer()
+
+
+@router.callback_query(Text(text=['10', '25', '40', '70', '100', 'over']))
+async def process_item_dimensions_cmd(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text=LEXICON_RU['dimension'],
+        reply_markup=item_dimensions_keyboard()
+    )
+    await callback.answer()
+
+
+@router.callback_query(Text(text=['empty_dimension', 'dimension_3', 'dimension_7', 'dimension_10']))
+async def process_rental_period_cmd(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text='Выберите срок аренды:',
+        reply_markup=rental_period_keyboard()
+    )
+
+
+@router.callback_query(Text(text=['one_month', 'tree_month', 'six_month', 'twelve_month', ]))
+async def get_phone_number(callback: CallbackQuery):
+    await callback.message.edit_text(
+        text='Введите ваш номер телефона для связи:',
+    )
+    await callback.answer()
--- keyboards/user_keyboards.py
+++ keyboards/user_keyboards.py
@@ -43,3 +43,86 @@ def what_can_be_stored_keyboard():
             ]
         ]
     )
+
+
+def send_to_storage_keyboard():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text='Привезете вещи сами?',
+                    callback_data='yourself'
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text='Курьером (бесплатно)',
+                    callback_data='courier'
+                )
+            ]
+        ]
+    )
+
+
+def item_weight_keyboard():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text='До 10 кг',
+                callback_data='10')],
+            [InlineKeyboardButton(
+                text='От 10 до 25 кг',
+                callback_data='25')],
+            [InlineKeyboardButton(
+                text='От 25 до 40 кг',
+                callback_data='40')],
+            [InlineKeyboardButton(
+                text='От 40 до 70 кг',
+                callback_data='70')],
+            [InlineKeyboardButton(
+                text='От 70 до 100 кг',
+                callback_data='100')],
+            [InlineKeyboardButton(
+                text='Свыше 100 кг',
+                callback_data='over')],
+        ]
+    )
+
+
+def item_dimensions_keyboard():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text='Не хочу! Я плачу бабки, измеряйте сами.',
+                callback_data='empty_dimension')],
+            [InlineKeyboardButton(
+                text='Менее 3 кв.м (1800 руб/мес)',
+                callback_data='dimension_3')],
+            [InlineKeyboardButton(
+                text='От 3 до 7 кв.м (2000 руб/мес)',
+                callback_data='dimension_7')],
+            [InlineKeyboardButton(
+                text='От 7 до 10 кв.м (2200 руб/мес)',
+                callback_data='dimension_10')]
+        ]
+    )
+
+
+def rental_period_keyboard():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text='1 месяц',
+                callback_data='one_month')],
+            [InlineKeyboardButton(
+                text='3 месяца',
+                callback_data='tree_month')],
+            [InlineKeyboardButton(
+                text='6 месяцев',
+                callback_data='six_month')],
+            [InlineKeyboardButton(
+                text='12 месяцев',
+                callback_data='twelve_month')],
+
+        ]
+    )
